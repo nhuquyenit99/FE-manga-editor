d1edc68920846f55b0e765c02c2d6726600bb0b0
nhuquyenit99 <nhimhana1404@gmail.com>
2021-12-28 00:14:26 +0700

    feat: text ediitor side bar

--- src/components/insert-text/index.tsx
+++ src/components/insert-text/index.tsx
@@ -1,46 +1,39 @@
 import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { Popover, InputNumber } from 'antd';
+import { RGBColor, SketchPicker } from 'react-color';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faBold, faItalic, faUnderline, faStrikethrough
+} from '@fortawesome/free-solid-svg-icons';
 import { Rnd } from 'react-rnd';
 import { Button } from 'antd';
 import './style.scss';
 
-const defaultSrc = require('https://github.com/nhuquyenit99/manga-editor/blob/master/src/assets/011.jpg');
+const defaultSrc = 'https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg';
 
-export const InsertTextPanel = () => {
-    return (
-        <div className='insert-text-panel-wrapper'>
-            <div className='header'>
-                <div className='title'>Insert Text</div>  
-            </div>
-            <div className='insert-text-panel'>
-                <div className='tool-menu'>
-                    <Button type='primary' shape='round'>Save</Button>
-                    <Button shape='round'>Cancel</Button>
-                </div>
-                <div className='workspace'>
-                    <div className='image-to-edit'>
-                        <img src={defaultSrc} alt='img-to-edit'/>
-                        <TextBox />
-                        {/* <input placeholder='Enter something...' className='text-input'/> */}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+const getRgbaFromString = (colorStr?: string) => {
+    if (!colorStr) {
+        return undefined;
+    }
+    const splittedStr = colorStr.split(',');
+    return {
+        r: splittedStr[0]?.slice(5),
+        g: splittedStr[1],
+        b: splittedStr[2],
+        a: splittedStr[3].slice(0, splittedStr[3].length - 1)
+    };
 };
 
-export type TextBoxRef = {
-    setBackground: (background?: string) => void
-    setTextColor: (textColor?: string) => void
-    setFontSize: (fontSize?: string) => void
-    setFontFamily: (fontFamily?: string) => void
-    setBold: (isBold: boolean) => void
-    setItalic: (isBold: boolean) => void
-    setUnderline: (isBold: boolean)  => void
-    setLineThrough: (isBold: boolean) => void
-}
+const getColorStrFromRgba = (color: RGBColor) => {
+    return `rgba(${color.r},${color.g},${color.b},${color.a})`;
+};
 
-export const TextBox = forwardRef((props, ref) => {
-    const [style, setStyle] = useState<React.CSSProperties>({});
+export const InsertTextPanel = () => {
+    const [style, setStyle] = useState<React.CSSProperties>({
+        background: 'rgba(255, 255, 255, 0.667)',
+        color: 'rgba(20,20,20,1)',
+        fontSize: '14px'
+    });
     const onSetBackGround = (background?: string) => {
         setStyle(prev => {
             return {
@@ -49,11 +42,11 @@ export const TextBox = forwardRef((props, ref) => {
             };
         });
     };
-    const onSetFontSize = (fontSize?: string) => {
+    const onSetFontSize = (fontSize?: number) => {
         setStyle(prev => {
             return {
                 ...prev,
-                fontSize: fontSize
+                fontSize: `${fontSize}px`
             };
         });
     };
@@ -76,52 +69,156 @@ export const TextBox = forwardRef((props, ref) => {
         });
     };
 
-    const onSetTextBold = (isBold: boolean = false) => {
+    const onToggleSetTextBold = () => {
         setStyle(prev => {
             return {
                 ...prev,
-                fontWeight: isBold ? 'bold' : 'normal'
+                fontWeight: prev.fontWeight === 'bold' ? 'normal' : 'bold'
             };
         });
     };
 
-    const onSetTextItalic = (isItalic: boolean = false) => {
+    const onToggleSetTextItalic = () => {
         setStyle(prev => {
             return {
                 ...prev, 
-                fontStyle: isItalic ? 'italic' : 'normal'
+                fontStyle: prev.fontStyle === 'italic' ? 'normal' : 'italic'
             };
         });
     };
 
-    const onSetTextUnderline = (isUnderline: boolean = false) => {
+    const onToggleSetTextUnderline = () => {
         setStyle(prev => {
             return {
                 ...prev, 
-                textDecoration: isUnderline ? 'underline' : 'normal'
+                textDecoration: prev.textDecoration === 'underline' ?  'none' : 'underline'
             };
         });
     };
 
-    const onSetTextLineThrough = (isLineThrough: boolean = false) => {
+    const onToggleSetTextLineThrough = () => {
         setStyle(prev => {
             return {
                 ...prev, 
-                textDecoration: isLineThrough ? 'line-through' : 'normal'
+                textDecoration: prev.textDecoration === 'line-through' ? 'none' : 'line-through'
             };
         });
     };
+    return (
+        <div className='insert-text-panel-wrapper'>
+            <div className='header'>
+                <div className='title'>Insert Text</div>  
+            </div>
+            <div className='insert-text-panel'>
+                <div className='text-tool-menu'>
+                    <h3>Text Style</h3>
+                    <div className='style-menu'>
+                        <Button className='style-btn' key='bold' 
+                            type={style.fontWeight === 'bold' ? 'primary' : undefined} 
+                            onClick={onToggleSetTextBold}>
+                            <FontAwesomeIcon icon={faBold}/>
+                        </Button>
+                        <Button className='style-btn' key='italic'
+                            type={style.fontStyle === 'italic' ? 'primary' : undefined}
+                            onClick={onToggleSetTextItalic}>
+                            <FontAwesomeIcon icon={faItalic}/>
+                        </Button>
+                        <Button className='style-btn' key='underline'
+                            type={style.textDecoration === 'underline' ? 'primary' : undefined}
+                            onClick={onToggleSetTextUnderline}
+                        >
+                            <FontAwesomeIcon icon={faUnderline}/>
+                        </Button>
+                        <Button className='style-btn' key='line-through'
+                            type={style.textDecoration === 'line-through' ? 'primary' : undefined}
+                            onClick={onToggleSetTextLineThrough}
+                        >
+                            <FontAwesomeIcon icon={faStrikethrough}/>
+                        </Button>
+                    </div>
+                    <h3>Text Color</h3>
+                    <Popover 
+                        trigger='click'
+                        placement='rightBottom'
+                        overlayClassName='pick-color-overlay'
+                        content={<SketchPicker color={getRgbaFromString(style.color) as any} 
+                            onChange={color => onSetTextColor(getColorStrFromRgba(color.rgb))} width='350px'/>}>
+                        <div style={{
+                            background: style.color,
+                            border: '5px solid white',
+                            width: '100px',
+                            height: '30px',
+                            cursor: 'pointer'
+                        }}>
+                        </div>
+                    </Popover>
+                    <h3>Background Color</h3>
+                    <Popover 
+                        trigger='click'
+                        placement='rightBottom'
+                        overlayClassName='pick-color-overlay'
+                        content={<SketchPicker color={getRgbaFromString(style.background as any) as any} 
+                            onChange={color => onSetBackGround(getColorStrFromRgba(color.rgb))} width='350px'/>}>
+                        <div style={{
+                            background: style.background,
+                            border: '5px solid white',
+                            width: '100px',
+                            height: '30px',
+                            cursor: 'pointer'
+                        }}>
+                        </div>
+                    </Popover>
+                    <h3>Font Size</h3>
+                    <div>
+                        <InputNumber min={1} 
+                            value={Number.parseInt((style.fontSize as string).slice(0,(style.fontSize as string).length - 2 ))}
+                            onChange={onSetFontSize}
+                            className='select-font-size'
+                        />
+                    </div>
 
-    useImperativeHandle(ref, () => ({
-        setBackground: onSetBackGround,
-        setTextColor: onSetTextColor,
-        setFontSize: onSetFontSize,
-        setFontFamily: onSetFontFamility,
-        setBold: onSetTextBold,
-        setItalic: onSetTextItalic,
-        setUnderline: onSetTextUnderline,
-        setLineThrough: onSetTextLineThrough
-    }));
+                    <Button type='primary' shape='round' className='btn-save'>Save</Button>
+                </div>
+                <div className='workspace'>
+                    <div className='image-to-edit'>
+                        <img src={defaultSrc} alt='img-to-edit'/>
+                        <TextBox style={style}/>
+                        {/* <input placeholder='Enter something...' className='text-input'/> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+// export type TextBoxRef = {
+//     setBackground: (background?: string) => void
+//     setTextColor: (textColor?: string) => void
+//     setFontSize: (fontSize?: string) => void
+//     setFontFamily: (fontFamily?: string) => void
+//     setBold: (isBold: boolean) => void
+//     setItalic: (isBold: boolean) => void
+//     setUnderline: (isBold: boolean)  => void
+//     setLineThrough: (isBold: boolean) => void
+// }
+
+type TextBoxProps = {
+    style: React.CSSProperties
+}
+
+export const TextBox = ({style}: TextBoxProps) => {
+   
+
+    // useImperativeHandle(ref, () => ({
+    //     setBackground: onSetBackGround,
+    //     setTextColor: onSetTextColor,
+    //     setFontSize: onSetFontSize,
+    //     setFontFamily: onSetFontFamility,
+    //     setBold: onSetTextBold,
+    //     setItalic: onSetTextItalic,
+    //     setUnderline: onSetTextUnderline,
+    //     setLineThrough: onSetTextLineThrough
+    // }));
 
     return (
         <Rnd
@@ -143,4 +240,4 @@ export const TextBox = forwardRef((props, ref) => {
             </div>
         </Rnd>
     );
-});
+};
